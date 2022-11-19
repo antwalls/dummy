e5976f71fb99fbd56b926b47f6ded7f2858e6efe
antwalls <antwalls92@gmail.com>
2022-11-19 14:24:48 +0100

    script python

---
+++ script.py
@@ -0,0 +1,100 @@
+# import datetime
+
+
+# mivariable: int
+# mivariablecadena : str = "12.5"
+# mivariableboolean: bool = True
+# mivariableDecimal: float = 1.2
+
+# mivariable = int()
+
+# type(mivariable)
+
+
+# mivariable3 = "sajjbdqjshdb"
+# # print(mivariable3)
+
+# mivariable3 = 1245
+# # print(mivariable3)
+
+# print(float(mivariablecadena) + mivariableDecimal)
+# print(float(mivariablecadena) * mivariableDecimal)
+
+# booleano1 = True
+# boleanno2 = False
+
+# print(booleano1 and boleanno2)
+# print(booleano1 or boleanno2)
+# print(booleano1 not(boleanno2) )
+
+
+# # tipos por valor tipos por referencia
+
+# def doble_valor(num: int) -> int:
+#     return num + num
+
+
+# def doble_valor(num: int) -> int:
+#     num = num + num
+#     return num
+
+# varible1 = 12
+# print(doble_valor(varible1))
+# print(varible1)
+
+
+
+
+# def doble_valores(num):
+#     num = num + num
+#     return num
+
+# variblelista = [1, 2, 3, 4, 6]
+
+# print(doble_valores(variblelista))
+# print(variblelista)
+
+# objeto = {"1":12, "2":"56"}
+
+# def modificar_objeto(obj):
+#     obj["3"] = "asdjh"
+#     return obj
+
+
+# print(modificar_objeto(objeto))
+# print(objeto)
+
+
+# lista : list = list()
+# lista = [2, "ghag", 2+ij, True, { } ]
+
+# lista = [2, 7 ,4 , 10 ,6 ]
+
+# def myFunct(e):
+#     return e
+
+
+# print(lista)
+ 
+# lista.sort(reverse=True)
+
+# print(lista)
+
+
+lista = [2, 7 ,4 , 10 , 6 ]
+# print( 7 in lista )
+# print(lista[1:4])
+
+dictinary = dict()
+dictinary = { 7 : "valor7" , 5 : "valor5"  }
+
+# print(7 in dictinary)
+# print(dictinary[7]) 
+
+print(list(dictinary.keys()))
+print(list(dictinary.values()))
+
+
+
+
+
